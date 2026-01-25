3cebd90126810d2222953e509a0eec150080991a
Martin Feilhauer <mfh@gnidam.net>
2026-01-25 15:51:49 +0100

    fps / delta

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +22,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        #print(dt)
 
 if __name__ == "__main__":
     main()
